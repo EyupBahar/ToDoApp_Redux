9bb42cdfd19d318719f19b6c6b486cf5951be964
7204-OMER <Eyupbhr24@gmail.com>
2021-06-02 17:34:35 +0300

    add index.js store and provider

--- src/index.js
+++ src/index.js
@@ -1,14 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { createStore } from "redux";
+import rootReducer from "./redux/index";
+import { Provider } from "react-redux";
+
+const store = createStore(rootReducer);
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider>
+      <App />
+    </Provider>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
---
+++ src/redux/index.js
@@ -0,0 +1,6 @@
+import { combineReducers } from "redux";
+import todoReducer from "./reducer/reducer";
+
+const rootReducer = combineReducers(todoReducer);
+
+export default rootReducer;
