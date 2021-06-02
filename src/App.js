868d7ff2ea4be1174e464d845e7ad7b3d64c4c1a
7204-OMER <Eyupbhr24@gmail.com>
2021-06-02 17:56:04 +0300

    add Css part

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import logo from "./logo.svg";
 import "./App.css";
+import TodoInput from "./component/TodoInput";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">test</header>
+      <h1>Clarus Todos</h1>
+      <TodoInput />
     </div>
   );
 }
