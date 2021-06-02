628324da39fb744b85544ef958ff3be4de1852a0
7204-OMER <Eyupbhr24@gmail.com>
2021-06-02 16:49:54 +0300

    add action js

---
+++ src/redux/actions/actions.js
@@ -0,0 +1,30 @@
+import {
+  ADD_TODO,
+  DELETE_TODO,
+  TOGGLE_TODO,
+  CLEAR_TODO_LİST,
+} from "../types/types";
+
+export const addTodo = (payload) => {
+  return {
+    type: ADD_TODO,
+    payload,
+  };
+};
+export const clearTodoList = () => {
+  return {
+    type: CLEAR_TODO_LİST,
+  };
+};
+export const deleteTodo = (payload) => {
+  return {
+    type: DELETE_TODO,
+    payload,
+  };
+};
+export const addTodo = (payload) => {
+  return {
+    type: TOGGLE_TODO,
+    payload,
+  };
+};
