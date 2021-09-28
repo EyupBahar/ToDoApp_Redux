8d51735c4e2199cc14a3ed9f64529cf14ff2f2d8
7204-OMER <Eyupbhr24@gmail.com>
2021-09-28 13:47:22 +0300

    add App.css

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
 import "./App.css";
 import TodoInput from "./components/TodoInput";
 import TodoList from "./components/TodoList";
+import todo3 from "./assets/todo3.jpeg";
 
 function App() {
   return (
     <div className="App">
-      <h1>My Todo</h1>
+      <h1 style={{ color: "white", fontSize: "10vh", fontFamily: "monospace" }}>
+        My Todo List
+      </h1>
       <TodoInput />
       <TodoList />
     </div>
