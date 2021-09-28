1b33e763cd480a6090d664b6a4dcd928f2b7e4ad
7204-OMER <Eyupbhr24@gmail.com>
2021-09-28 11:59:12 +0300

    add Todolist

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { useSelector, useDispatch } from "react-redux";
-import { clearTodoList } from "../redux/actions/actions";
+import { clearTodoList, filterTodos } from "../redux/actions/actions";
 import TodoItem from "./TodoItem";
 
 const Todolist = () => {
@@ -9,6 +9,9 @@ const Todolist = () => {
 
   const handleClearList = () => {
     dispatch(clearTodoList());
+  };
+  const handleFilterCompleted = () => {
+    dispatch(filterTodos());
   };
   return (
     <div>
@@ -26,10 +29,26 @@ const Todolist = () => {
           fontSize: "1rem",
           backgroundColor: "#ac0d0d",
           color: "white",
+          cursor: "pointer",
           borderRadius: "10px",
         }}
       >
         Clear
+      </button>
+      <button
+        onClick={handleFilterCompleted}
+        style={{
+          height: "50px",
+          width: "100px",
+          marginTop: "25px",
+          fontSize: "1rem",
+          backgroundColor: "#33B744",
+          color: "white",
+          cursor: "pointer",
+          borderRadius: "10px",
+        }}
+      >
+        Completed
       </button>
     </div>
   );
